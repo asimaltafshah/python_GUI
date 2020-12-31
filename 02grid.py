53709a8532d0e868a0fc8c53044fdd226d66a647
Asim Altaf Shah <asimaltafshah@gmail.com>
2020-12-31 15:36:20 +0500

    adding grid

---
+++ 02grid.py
@@ -0,0 +1,9 @@
+from tkinter import *
+root = Tk()
+
+myLabel1 = Label(root, text = "hello world!").grid(row =0, column =0)
+myLabel2 = Label(root, text = "My Name is Asim Altaf").grid(row =1, column =0)
+
+
+
+root.mainloop()
